dbd79b0b53ad112adfcef592d6641039d873e7a4
yoyojyv <yoyojyv@gmail.com>
2013-12-24 13:30:45 +0900

    03. Post 추가, 수정, 삭제 기능 만들기

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -18,4 +18,58 @@ class PostsController extends AppController {
         }
         $this->set('post', $post);
     }
+
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->Post->create();
+            if ($this->Post->save($this->request->data)) {
+                $this->Session->setFlash(__('Your post has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            }
+            $this->Session->setFlash(__('Unable to add your post.'));
+        }
+
+        // $this->request->data
+    }
+
+
+    public function edit($id = null) {
+        if (!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        $post = $this->Post->findById($id);
+        if (!$post) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        if ($this->request->is(array('post', 'put'))) {
+            $this->Post->id = $id;
+            if ($this->Post->save($this->request->data)) {
+                $this->Session->setFlash(__('Your post has been updated.'));
+                return $this->redirect(array('action' => 'index'));
+            }
+            $this->Session->setFlash(__('Unable to update your post.'));
+        }
+
+        if (!$this->request->data) {
+            $this->request->data = $post;
+        }
+    }
+
+
+    public function delete($id) {
+        if ($this->request->is('get')) {
+            throw new MethodNotAllowedException();
+        }
+
+        if ($this->Post->delete($id)) {
+            $this->Session->setFlash(
+                __('The post with id: %s has been deleted.', h($id))
+            );
+            return $this->redirect(array('action' => 'index'));
+        }
+    }
+
+
 }
